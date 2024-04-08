9da3782fe6b0a8f790e1ad1d50d139cc9fe74f82
Kara <karaulf@uw.edu>
2024-04-08 14:52:07 -0700

    removing file and adding proper files for Week 2

---
+++ Week2/MyVariable1.java
@@ -0,0 +1,35 @@
+public class MyVariable1 {
+    public static void main(String[] args){
+        String name = "Kara"; // after statement, don't forget semicolon
+        int hours = 40; //assigning 40 to the variable "hours"
+        // String has to have "" nested
+        // int or numbers does not need ""
+        System.out.println(name);
+        System.out.println(hours);
+
+//        String building, tree, lake, ocean;
+//        tree = "Oak";
+//        building = "Space Needle";
+//        lake = "Crater Lake";
+//        ocean = "Pacific";
+
+        String building = "Space Needle", tree = "Oak", lake = "Crater Lake", ocean = "Pacific";
+
+//        int weeks, decades, months, years;
+//        weeks = 4;
+//        decades = 10;
+//        months = 3;
+//        years = 5;
+//
+        int weeks = 4, decades = 10, months = 3, years = 5;
+
+        System.out.println(tree);
+        System.out.println(building);
+        System.out.println(lake);
+        System.out.println(ocean);
+        System.out.println(weeks);
+        System.out.println(decades);
+        System.out.println(months);
+        System.out.println(years);
+    }
+}
--- Week2/concat.java
+++
@@ -1,7 +0,0 @@
-public class concat.java {
-    public static void main(String[] args) {
-        String color = "green";
-        int money = 200;
-        System.out.println(color + money);
-    }
-}

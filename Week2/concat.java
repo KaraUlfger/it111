2b4d08c58129e499f4abbfb1345a958e57d19bef
Kara <karaulf@uw.edu>
2024-04-07 19:33:24 -0700

    upload file and update

---
+++ Week2/concat.java
@@ -0,0 +1,7 @@
+public class concat.java {
+    public static void main(String[] args) {
+        String color = "green";
+        int money = 200;
+        System.out.println(color + money);
+    }
+}

d9cf2e3450f04d45a5e6963e39b503c00f37157f
Kara <karaulf@uw.edu>
2024-05-03 13:51:38 -0700

    removing redundant code

--- Week5/src/DailySpecials.java
+++ Week5/src/DailySpecials.java
@@ -29,50 +29,79 @@ public class DailySpecials {
                 System.out.println(specials + "'s coffee of the day is a " + coffee + " and the price is $" + price);
                 System.out.println("How many " + coffee + "s would you like?");
                 totalCoffee = total.nextInt();
+                if(totalCoffee ==0){
+                    System.out.println("Looks like you do not like "+coffee+"s.");
+                } else if (totalCoffee==1) {
+                    System.out.println("You will be ordering only 1 "+coffee+" today!");
+                }else {
                 System.out.println(totalCoffee + " " + coffee + "s have been ordered, and the total is ");
                 System.out.print("$");
                 System.out.printf("%.2f", totalCoffee * price);
-                System.out.print(" dollars!");
+                System.out.print(" dollars!");}
                 break;
             case "Tuesday":
                 coffee= "Americano";
                 price=2.95;
                 System.out.println(specials+"'s coffee of the day is a "+coffee+ " and the price is $"+price);
+                System.out.println("How many " + coffee + "s would you like?");
                 totalCoffee = total.nextInt();
+                if(totalCoffee ==0){
+                    System.out.println("Looks like you do not like "+coffee+"s.");
+                } else if (totalCoffee==1) {
+                    System.out.println("You will be ordering only 1 " + coffee + " today!");
+                }else{
                 System.out.println(totalCoffee + " " + coffee + "s have been ordered, and the total is ");
                 System.out.print("$");
                 System.out.printf("%.2f", totalCoffee * price);
-                System.out.print(" dollars!");
+                System.out.print(" dollars!");}
                 break;
             case "Wednesday":
                 coffee= "Frappe";
                 price=5.95;
                 System.out.println(specials+"'s coffee of the day is a "+coffee+ " and the price is $"+price);
+                System.out.println("How many " + coffee + "s would you like?");
                 totalCoffee = total.nextInt();
+                if(totalCoffee ==0){
+                    System.out.println("Looks like you do not like "+coffee+"s.");
+                }else if (totalCoffee==1) {
+                    System.out.println("You will be ordering only 1 " + coffee + " today!");
+                }else{
                 System.out.println(totalCoffee + " " + coffee + "s have been ordered, and the total is ");
                 System.out.print("$");
                 System.out.printf("%.2f", totalCoffee * price);
-                System.out.print(" dollars!");
+                System.out.print(" dollars!");}
                 break;
             case "Thursday":
                 coffee= "Drip";
                 price=1.95;
                 System.out.println(specials+"'s coffee of the day is a "+coffee+ " and the price is $"+price);
+                System.out.println("How many " + coffee + "s would you like?");
                 totalCoffee = total.nextInt();
+                if(totalCoffee ==0){
+                    System.out.println("Looks like you do not like "+coffee+"s.");
+                }else if (totalCoffee==1) {
+                    System.out.println("You will be ordering only 1 " + coffee + " today!");
+                }else{
                 System.out.println(totalCoffee + " " + coffee + "s have been ordered, and the total is ");
                 System.out.print("$");
                 System.out.printf("%.2f", totalCoffee * price);
-                System.out.print(" dollars!");
+                System.out.print(" dollars!");}
                 break;
             case "Friday":
                 coffee= "Iced Mocha";
                 price=3.95;
                 System.out.println(specials+"'s coffee of the day is a "+coffee+ " and the price is $"+price);
+                System.out.println("How many " + coffee + "s would you like?");
                 totalCoffee = total.nextInt();
+                if(totalCoffee ==0){
+                    System.out.println("Looks like you do not like "+coffee+"s.");
+                }else if (totalCoffee==1) {
+                    System.out.println("You will be ordering only 1 " + coffee + " today!");
+                }else{
                 System.out.println(totalCoffee + " " + coffee + "s have been ordered, and the total is ");
                 System.out.print("$");
                 System.out.printf("%.2f", totalCoffee * price);
-                System.out.print(" dollars!");
+                System.out.print(" dollars!");}
                 break;
 
 //            default:
